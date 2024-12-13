ee5e4586b6747500a487c495d09580a992a6e017
mnkjp <menkejp@mail.uc.edu>
2024-12-13 16:53:25 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       final double TAX_RATE = .05;
+        int price = 0;
+        double taxAmount = 0;
+
+                System.out.println(":Enter the Price of the item ");
+                        price = 10;
+                        taxAmount = price * TAX_RATE;
+                                System.out.println("Your total price is " + (taxAmount + price));
+
+
+    }
+}
